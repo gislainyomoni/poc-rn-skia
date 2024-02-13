fa05fb723af275b4057d4b0eaf84eb3bdbad59fa
gmoreira <gislain.moreira@yomoni.fr>
2024-02-13 20:29:04 +0100

    add getYforX in line chart

--- src/Chart/components/CharLine/ChartLine.tsx
+++ src/Chart/components/CharLine/ChartLine.tsx
@@ -6,7 +6,7 @@ import { IChartLineProps } from './ChartLine.props'
 import { useChart } from '../../lib/chart.context'
 
 export const ChartLine = memo<IChartLineProps>(({ data }) => {
-  const { scaleY, scaleX, scrollX, rangeX, rangeY } = useChart()
+  const { scaleY, scaleX, scrollX, rangeX, rangeY, invertX } = useChart()
 
   const path = useMemo(() => {
     const result = Skia.Path.Make()
@@ -28,6 +28,25 @@ export const ChartLine = memo<IChartLineProps>(({ data }) => {
   const inactiveX = useDerivedValue(() => Math.max(scrollX.value, minX))
   const inactiveWidth = useDerivedValue(() => maxX - inactiveX.value)
 
+  const cursorY = useDerivedValue(() => {
+    const x = invertX(scrollX.value)
+    let p1 = { x: data[0].x, y: data[0].y }
+    let p2 = { x: p1.x, y: p1.y }
+    for (let i = 1; i < data.length; i++) {
+      p2 = data[i]
+      if (x > p1.x && x <= p2.x) {
+        const { x: x1, y: y1 } = p1
+        const { x: x2, y: y2 } = p2
+        // console.log('points', p1, p2)
+        const y = y1 + ((x - x1) / (x2 - x1)) * (y2 - y1)
+        // console.log('y', y)
+        return scaleY(y)
+      }
+      p1 = data[i]
+    }
+    return scaleY(data[0].y)
+  })
+
   return (
     <Group>
       <Mask mask={<Path path={path} strokeWidth={2} style="stroke" />}>
@@ -45,7 +64,7 @@ export const ChartLine = memo<IChartLineProps>(({ data }) => {
       {data.map(({ x, y }, index) => (
         <Circle key={index} r={4} cx={scaleX(x)} cy={scaleY(y)} color="black" style="fill" />
       ))}
-      <Circle r={10} cx={scrollX} cy={100} color="green" style="fill" />
+      <Circle r={10} cx={scrollX} cy={cursorY} color="green" style="fill" />
     </Group>
   )
 })
